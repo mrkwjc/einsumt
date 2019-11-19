6c2fc39f153f92a4b9fe516d033731d8aa0938fe
Marek Wojciechowski <mrkwjc@gmail.com>
2019-11-19 10:32:12 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="einsumt",
+    version="0.9.0",
+    author="Marek Wojciechowski",
+    author_email="mrkwjc@gmail.com",
+    description="Multithreaded version of numpy.einsum function",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mrkwjc/einsumt",
+    packages=setuptools.find_packages(),
+    keywords=['numpy', 'einsum', 'hpc'],
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+    py_modules=['einsumt']
+)
